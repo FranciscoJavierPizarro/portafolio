950a1a034b34a5dd8419001cbd6075149cc09343
Francisco Javier Pizarro <pizarrojavier000@gmail.com>
2022-05-01 12:52:39 +0200

    Google analytics funcionando

--- pages/_app.js
+++ pages/_app.js
@@ -5,13 +5,13 @@ function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page)
   return <>
   
-  <Script id="ga1"strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_KEY}"/>
-  <Script id="ga2"strategy="afterInteractive">
+  <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_KEY}"/>
+  <Script id="google analytics"strategy="afterInteractive">
     {`
        window.dataLayer = window.dataLayer || [];
        function gtag(){dataLayer.push(arguments);}
        gtag('js', new Date());
-       gtag('config', ${process.env.NEXT_PUBLIC_ANALYTICS_KEY});
+       gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS_KEY}');
     `}
   </Script>
   {getLayout(<Component {...pageProps} />)}
